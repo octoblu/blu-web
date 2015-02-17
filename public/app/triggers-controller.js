ac815879d6c0b0db61242a3c994f2027825a0721
Alisa "Awesome" Palson and Roy van de Water <sqrt+alisa+roy@octoblu.com>
2015-02-17 16:35:36 -0700

    Remove old controller and service

--- public/app/triggers-controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('blu')
-  .controller('triggersController', function($scope, meshbluService){
-
-    meshbluService.getConnection(function(conn){
-      console.log('Connected');
-    });
-  });
